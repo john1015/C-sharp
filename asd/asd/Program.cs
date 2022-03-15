a4493821b2417122cc7c952a8970c3cfd7f36f43
john1015 <10151404john@gmail.com>
2022-03-15 20:30:15 +0000

    3월15일

---
+++ asd/asd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            if (year < 1 || year > 4000) return;
+
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                Console.WriteLine("윤년입니다.");
+            }
+           
+            else
+            {
+                Console.WriteLine("윤년이아닙니다");
+            }
+        }
+    }
+}
