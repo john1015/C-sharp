49b67008a9f0780bc819dbfcacb2a5be793447a5
john1015 <10151404john@gmail.com>
2022-04-05 20:30:15 +0000

    4월5일

---
+++ 0405/0405/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0405
+{
+    class A
+    {
+        public String name;
+    }
+    class B : A
+    {
+        public B(String name)
+        {
+            base.name = name;
+        }
+        public void printName()
+        {
+            Console.WriteLine(name);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            B hr = new B("상속");
+            hr.printName();
+        }
+    }
+}
